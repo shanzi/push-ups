fd8362b5160e1b65207829f53d4345fa4ad69b7e
Chase Zhang <yun.er.run@gmail.com>
2013-11-10 00:56:10 +0800

    add description param to form

--- src/push_ups/forms.clj
+++ src/push_ups/forms.clj
@@ -1,5 +1,6 @@
 (ns push-ups.forms
-  (:use hiccup.form)) 
+  (:use hiccup.form
+        hiccup.element)) 
 
 (defn- start-week
   "dropdown for selecting start date"
@@ -57,18 +58,26 @@
 
 (defn exercise-time-choice-set
   ([legend]
-   (exercise-time-choice-set legend "next-week" "1-3-5" 20))
-  ([legend week-choice day-choice time-choice]
+   (exercise-time-choice-set legend nil))
+  ([legend description]
+   (exercise-time-choice-set legend "next-week" "1-3-5" 20 description))
+  ([legend week-choice day-choice time-choice description]
    [:fieldset.time-choice-set [:legend legend]
+    (when-not (or (nil? description) (false? description))
+      (into [:p]description))
     (start-week week-choice)
     (day-in-week day-choice)
     (time-in-day time-choice)]))
 
 (defn test-result-set
   ([legend]
-   (test-result-set legend 0))
-  ([legend checked-idx]
+   (test-result-set legend nil))
+  ([legend description]
+   (test-result-set legend 0 description))
+  ([legend checked-idx description]
    [:fieldset.result-set [:legend legend] 
+    (when-not (or (nil? description) (false? description)) 
+      (into [:p] description))
     (into [:table [:tr [:th ""] [:th "Your Age"] [:th {:width "60%"} "How many push-ups did you do?"]]]
           (map-indexed (fn [idx [label sep]] 
                          (test-input-row label idx (= idx checked-idx) sep))
@@ -78,8 +87,16 @@
   "generate initial form for creating a new calendar"
   [action]
   (form-to [:post action]
-           (exercise-time-choice-set "Arrange exercise time")
-           (test-result-set "Input initial test result")
+           (exercise-time-choice-set "Arrange exercise time"
+                                     "Chose the date and time you'd like to arrange your push-ups exercise at.
+                                     You will be able to change the settings after taken the first periodic test 
+                                     two weeks later.")
+           (test-result-set "Input initial test result"
+                            [nil 
+                             "Before your begin your exercise, try to execute as many good-form push ups as you can
+                            and count the number. Please click "
+                            (link-to "http://www.hundredpushups.com/test.html" "here")
+                            " for more infomation."])
            [:p.form-controllers 
             (submit-button "Submit")
             (reset-button "Reset")]))
